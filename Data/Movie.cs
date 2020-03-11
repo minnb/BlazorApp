dd31b2bc22e6bea54892362b7144dc46b7dc7ba6
minnb <minhnb.it@gmail.com>
2020-03-11 07:37:12 +0700

    A

---
+++ Data/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Data
+{
+    public class MovieModel
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public DateTime Release { get; set; }
+    }
+}
